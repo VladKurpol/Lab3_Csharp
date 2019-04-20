1446e684f83cd543d20caf830e13a84a9ffe6702
VladKurpol <48331088+VladKurpol@users.noreply.github.com>
2019-04-20 12:20:27 +0300

    SAD

--- TheThirdLab_FirstCourse/Properties/AssemblyInfo.cs
+++ TheThirdLab_FirstCourse/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Lab3")]
+[assembly: AssemblyTitle("TheThirdLab_FirstCourse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab3")]
+[assembly: AssemblyProduct("TheThirdLab_FirstCourse")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("4a34ef9d-e9c5-42ac-84f5-739e28e6b774")]
+[assembly: Guid("784e4b9a-c4cd-417a-8f27-fcb6ce7096bc")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
